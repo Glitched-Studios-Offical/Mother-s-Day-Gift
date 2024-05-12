935ebcda248cf442d30864392cce55286a4778f3
Glitched Da Cat <143963110+glitchedtahcat@users.noreply.github.com>
2024-05-11 21:56:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const glowyText = document.querySelector('.glowy-text');
 let hue = 0; 
 setInterval(() => { 
 	const heart = document.createElement('span');
-	const heartColors = ['❤️', '💛', '💚', '💙', '💜']; // Define an array of heart colors
-	const randomHeartColor = heartColors[Math.floor(Math.random() * heartColors.length)]; // Select a random heart color
+	const heartColors = ['❤️', '💛', '💚', '💙', '💜']; 
+	const randomHeartColor = heartColors[Math.floor(Math.random() * heartColors.length)]; 
 	heart.innerText = randomHeartColor; 
 	heart.style.position = 'absolute';
 	heart.style.left = `${Math.random() * window.innerWidth}px`;
@@ -13,4 +13,4 @@ setInterval(() => {
 	heart.style.color = `hsl(${hue}, 100%, 50%)`; 
 	document.body.appendChild(heart);
 	hue = (hue + 1) % 360; 
-}, 300); 
+}, 300); // 300 = 300ms and to change it to sec it is 3000
